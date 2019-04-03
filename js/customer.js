7049ca2a14d2b82404794445959fe32fb5c6c425
Manuel Guarniz Cruz <cruzemg95@gmail.com>
2019-04-03 11:15:53 -0500

    remove files to push new proyecto

--- js/customer.js
+++
@@ -1,7 +0,0 @@
-function PrintBill(imagen) {
-    newWindow = window.open("","Imagenes","width=400,height=450,left=100,top=60");
-    newWindow.document.open();
-    newWindow.document.write('<html><head></head><body onload="window.print()"><img src="'+ imagen +'"/></body></html>');  
-    newWindow.document.close();
-    newWindow.focus();
-}
